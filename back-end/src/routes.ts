9dcdc7f879cc730ca9799fb175f993430d5100e0
GABRIEL LUCAS <119793695+gabriellglrs@users.noreply.github.com>
2023-12-24 09:50:56 -0300

    back end v1

---
+++ back-end/src/routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from "fastify";
+
+export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
+
+    fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
+        
+        return { ok: true }
+    });
+};
